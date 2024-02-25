5689366fc5da73bb170d8d38d8c076ceb0a24ac7
William <willspri@gmail.com>
2024-02-25 14:51:38 +1300

    added

---
+++ sorting.java
@@ -0,0 +1,103 @@
+import java.util.Arrays;
+
+public class SortingAlgorithms {
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int mid = (l + r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+        
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        
+        // Bubble Sort
+        bubbleSort(arr.clone());
+        System.out.println("Bubble Sort: " + Arrays.toString(arr));
+        
+        // Selection Sort
+        selectionSort(arr.clone());
+        System.out.println("Selection Sort: " + Arrays.toString(arr));
+        
+        // Merge Sort
+        mergeSort(arr.clone(), 0, arr.length - 1);
+        System.out.println("Merge Sort: " + Arrays.toString(arr));
+    }
+}
